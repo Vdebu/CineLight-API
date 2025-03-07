4ff2dec63edc7d8f19fde526369a7b86403cd0b8
Vdebu <vdebuzzz@gmail.com>
2025-03-07 14:41:13 +0800

    进行基础的json编码：将json编码的代码逻辑抽象成方法加入helper模块。gin在进行json格式转换时是会判断格式并返回错误的,如果格式不正确会抛出错误并停止执行后续的所有代码，所以不需要人为检测传入数据的正确与否。

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -7,7 +7,25 @@ import (
 
 // 返回服务器的状态 运行环境 版本
 func (app *application) healthcheckHandler(c *gin.Context) {
-	c.String(http.StatusOK, "status:available\n")
-	c.String(http.StatusOK, "environment:%s\n", app.config.env)
-	c.String(http.StatusOK, "version:%s\n", version)
+	// 定义raw json
+	//js := `{"status":"available","environment":%q,"version":%q}`
+	// 初始化json
+	// 也可以通过gin.H{"status":"available","environment":app.config.env,"version":version}进行定义
+	//js = fmt.Sprintf(js, app.config.env, version)
+
+	// 设置表头参数告诉客户端输送的是json数据
+	//c.Header("Content-Type", "application/json")
+	// 像响应体输出json数据 会自动设置表头参数 Content-Type as "application/ json".
+	// c.String(http.StatusOK, js) 在这里用标准string输出浏览器才会自动检测为json格式 而不能用c.Json输出
+
+	// c.Json同样可以解析map[string]string -- map[string]string
+	//data := map[string]string{
+	//	"status":      "available",
+	//	"environment": app.config.env,
+	//	"version":     version,
+	//}
+	//data := make(chan string)
+	data := gin.H{"status": "available", "environment": app.config.env, "version": version}
+	// 自动初始化并输出json数据
+	app.writeJson(c, http.StatusOK, data, nil)
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 	"strconv"
+	"strings"
 )
 
 // 将用于提取id的代码逻辑抽象成一个模块
@@ -14,4 +16,25 @@ func (app *application) readIDParam(c *gin.Context) (int64, error) {
 		return 0, err
 	}
 	return id, nil
+}
+
+// 发送json数据到响应体
+func (app *application) writeJson(c *gin.Context, status int, data interface{}, header http.Header) {
+	// 数据初始化问题 不能确定传入的数据是否符合json格式 gin也没有提供内置函数进行判断
+	// gin的c.Json也是调用json.Marshal但是不会自动检查并报告错误
+	// 进行实际操作前进行人为调用json.Marshal并进行错误检测
+	//if _, err := json.Marshal(data); err != nil {
+	//	c.JSON(http.StatusInternalServerError, gin.H{"error": "invalid json data"})
+	//	return
+	//}
+	// 确定是能转换成json格式后 将提供的数据转换成json并输出到响应体中
+
+	// gin在进行json格式转换时是会判断格式并返回错误的 如果格式不正确会抛出错误并停止执行后续的所有代码
+	c.JSON(status, data)
+	// 向响应体中添加传入的表头
+	for key, val := range header {
+		// 将 []string 转换为 string，多个值以逗号分隔
+		c.Header(key, strings.Join(val, ","))
+	}
+	//app.logger.Println("we are here...")
 }
