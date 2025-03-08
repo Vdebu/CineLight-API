1d4b54947777bffd391d6c703bbdefa3fbcc16bf
Vdebu <vdebuzzz@gmail.com>
2025-03-08 17:09:40 +0800

    将字段有效性检测抽象成新方法：使代码更加简洁灵活，增强代码的复用率。

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -23,27 +23,16 @@ func (app *application) createMovieHandler(c *gin.Context) {
 		app.badRequestResponse(c, err)
 		return
 	}
-	// 数据提取成功后初始化校验器
+	// 数据提取成功后初始化校验器 以后可能会有各种各样的校验器
 	v := validator.New()
-	// 对输入的各个字段进行检查
-	v.Check(input.Title != "", "title", "must be provided")
-	v.Check(len(input.Title) <= 500, "title", "must not be more than 500 bytes long")
-
-	v.Check(input.Year != 0, "year", "must be provided")
-	v.Check(input.Year >= 1888, "year", "must be greater than 1888")
-	v.Check(input.Year <= int32(time.Now().Year()), "year", "year must not be in the future")
-
-	v.Check(input.Runtime != 0, "runtime", "must be provided")
-	v.Check(input.Runtime > 0, "runtime", "must be positive integer")
-
-	v.Check(input.Genres != nil, "genres", "must be provided")
-	v.Check(len(input.Genres) >= 1, "genres", "must contain at least 1 genre")
-	v.Check(len(input.Genres) <= 5, "genres", "must not contain more than 5 genres")
-	// 查看是否有标签是重复的
-	v.Check(validator.Unique(input.Genres), "genres", "must not contain duplicate values")
-	// 检查是否通过检测
-	if !v.Valid() {
-		// 输出错误信息并结束当前请求
+	// 将输入的信息载入Movie结构体用于后续检测
+	movie := &data.Movie{
+		Title:   input.Title,
+		Year:    input.Year,
+		Runtime: input.Runtime,
+		Genres:  input.Genres,
+	}
+	if data.ValidateMovie(v, movie); !v.Valid() {
 		app.failedValidationResponse(c, v.Errors)
 		return
 	}
--- internal/data/movies.go
+++ internal/data/movies.go
@@ -1,6 +1,9 @@
 package data
 
-import "time"
+import (
+	"greenlight.vdebu.net/internal/validator"
+	"time"
+)
 
 // 使用结构体存储基本信息
 type Movie struct {
@@ -11,4 +14,25 @@ type Movie struct {
 	Runtime   Runtime   `json:"runtime,omitempty"` // 时长 使用自定义类型存储播放时长(实现了json.Marshal接口)生成自定义的格式化信息
 	Genres    []string  `json:"genres,omitempty"`  // 标签 对发行时间时长标签进行空值隐藏("",0,nil或空slice,map)
 	Version   int32     `json:"version"`           // 版本信息从1开始 当电影信息更新版本信息会自动递增
+}
+
+// 用于检测电影结构体的各个字段是否有效
+func ValidateMovie(v *validator.Validator, movie *Movie) {
+	// 对输入的各个字段进行检查
+	v.Check(movie.Title != "", "title", "must be provided")
+	v.Check(len(movie.Title) <= 500, "title", "must not be more than 500 bytes long")
+
+	v.Check(movie.Year != 0, "year", "must be provided")
+	v.Check(movie.Year >= 1888, "year", "must be greater than 1888")
+	v.Check(movie.Year <= int32(time.Now().Year()), "year", "year must not be in the future")
+
+	v.Check(movie.Runtime != 0, "runtime", "must be provided")
+	v.Check(movie.Runtime > 0, "runtime", "must be positive integer")
+
+	v.Check(movie.Genres != nil, "genres", "must be provided")
+	v.Check(len(movie.Genres) >= 1, "genres", "must contain at least 1 genre")
+	v.Check(len(movie.Genres) <= 5, "genres", "must not contain more than 5 genres")
+	// 查看是否有标签是重复的
+	v.Check(validator.Unique(movie.Genres), "genres", "must not contain duplicate values")
+
 }
