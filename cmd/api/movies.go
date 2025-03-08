f05d142d1e5ce4abae15e54a2cf1835f62f7b492
Vdebu <vdebuzzz@gmail.com>
2025-03-08 13:56:57 +0800

    对POST请求的JSON数据进行解析：使用Gin内置的ShouldBindJSON进行解析并将数据绑定到结构体上。

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"greenlight.vdebu.net/internal/data"
 	"net/http"
@@ -8,7 +9,22 @@ import (
 )
 
 func (app *application) createMovieHandler(c *gin.Context) {
-	c.String(http.StatusOK, "create a new movie...")
+	// 创建结构体存储待解析的JSON数据 这个结构体是完整Movie结构体的子集
+	var input struct {
+		Title   string   `json:"title"`
+		Year    int32    `json:"year"`
+		Runtime int32    `json:"runtime"`
+		Genres  []string `json:"genres"`
+	}
+	// 使用Gin的内置方法对请求体中的JSON数据进行解析
+	if err := c.ShouldBindJSON(&input); err != nil {
+		// 向响应体中输出错误信息
+		app.errorResponse(c, http.StatusBadRequest, err.Error())
+	}
+	// 使用Windows发送 必须将引号转义
+	// curl -i -d "{\"title\":\"Moana\",\"year\":2016,\"runtime\":107,\"genres\":[\"animation\",\"adventure\"]}" http://localhost:3939/v1/movies
+	// 将提取到的信息作为响应体输出
+	fmt.Fprintf(c.Writer, "%+v\n", input)
 }
 func (app *application) showMovieHandler(c *gin.Context) {
 	// 使用抽象出来的数据读取模块提取id
