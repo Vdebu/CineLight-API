697f8810bd67de3b2f7d1ad2db73553903f4f98b
Vdebu <vdebuzzz@gmail.com>
2025-03-09 14:33:04 +0800

    连接到PostgreSQL：将数据库DSN解耦存储到本地的环境变量中，初始化DSN的时候读取使用，提高可移植性。

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"time"
+
+	_ "github.com/lib/pq" //隐式导入sql驱动
 )
 
 // 定义当前版本信息 后续会使用自动生成的手段进行改进
@@ -14,10 +18,11 @@ const version = " 1.0.0"
 
 // 存储服务器的配置信息
 type config struct {
-	// 端口
-	port int
-	// 运行环境
-	env string
+	port int    // 端口
+	env  string // 运行环境
+	db   struct {
+		dsn string // 在服务器配置中存储dsn
+	}
 }
 
 // 注入依赖
@@ -33,11 +38,23 @@ func main() {
 	flag.IntVar(&cfg.port, "port", 3939, "API server port")
 	// 服务器的环境信息
 	flag.StringVar(&cfg.env, "env", "development", "Environment(development|staging|production)")
+	// 默认从系统的环境变量中获取服务器的数据库DSN(data source name)
+	// PostgreSQL驱动可能会使用 SSL连接如果服务器没有启用 SSL需要在 DSN 中添加参数来禁用SSL
+	flag.StringVar(&cfg.db.dsn, "db-dsn", os.Getenv("GREENLIGHT_DB_DSN"), "PostgreSQL DSN")
 	// 解析命令行参数
 	flag.Parse()
 	// 初始化服务器内部的日志工具
 	// 输出时间与日期
 	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+	//logger.Println("dsn:", cfg.db.dsn)
+	// 初始化数据库链接
+	db, err := openDB(cfg)
+	if err != nil {
+		logger.Fatal(err)
+	}
+	// 程序结束时关闭数据库的连接
+	defer db.Close()
+	logger.Println("db connection established...")
 	// 初始化依赖
 	app := &application{
 		config: cfg,
@@ -56,6 +73,27 @@ func main() {
 	}
 	// 启动服务器
 	logger.Printf("starting %s server on %s\n", cfg.env, srv.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Fatal(err)
+}
+
+// 尝试连接数据库 返回数据库连接池sql.DB
+func openDB(cfg config) (*sql.DB, error) {
+	// 传入数据库信息
+	db, err := sql.Open("postgres", cfg.db.dsn)
+	if err != nil {
+		return nil, err
+	}
+	// 创建Context 在5秒连接超时后关闭连接
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	// 超时后关闭连接
+	defer cancel()
+
+	// 检查连接是否建立
+	// 如果在5秒内没有ping成功就会返回错误
+	err = db.PingContext(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
